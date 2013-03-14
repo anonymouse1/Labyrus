232d7746dbb5cbc4dcd8555c5785d62a7c24ea1c
Tyulbashev Vladislav Sergeevich <vladislav.tyulbashev@yandex.ru>
2013-03-14 21:50:18 +0400

    Translation file

---
+++ Client/ru_RU.ts
@@ -0,0 +1,145 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>Connection</name>
+    <message>
+        <location filename="connection.ui" line="26"/>
+        <source>Connecting</source>
+        <translation type="unfinished">Подключение</translation>
+    </message>
+    <message>
+        <location filename="connection.ui" line="32"/>
+        <source>Connecting to</source>
+        <translation type="unfinished">Подключаемся к</translation>
+    </message>
+    <message>
+        <location filename="connection.ui" line="39"/>
+        <source>Please wait...</source>
+        <translation type="unfinished">Пожалуйста, ждите...</translation>
+    </message>
+</context>
+<context>
+    <name>DrawGl</name>
+    <message>
+        <location filename="drawgl.cpp" line="552"/>
+        <source>From start game: </source>
+        <translation type="unfinished">Со старта игры:</translation>
+    </message>
+    <message>
+        <location filename="drawgl.cpp" line="553"/>
+        <source>Alive status: </source>
+        <translation type="unfinished">Живаой:</translation>
+    </message>
+    <message>
+        <location filename="drawgl.cpp" line="554"/>
+        <source>patrons: </source>
+        <translation type="unfinished">Патронов:</translation>
+    </message>
+    <message>
+        <location filename="drawgl.cpp" line="555"/>
+        <source>walls: </source>
+        <translation type="unfinished">Стен:</translation>
+    </message>
+    <message>
+        <location filename="drawgl.cpp" line="556"/>
+        <source>destroy: </source>
+        <translation type="unfinished">Гранат:</translation>
+    </message>
+    <message>
+        <location filename="drawgl.cpp" line="557"/>
+        <source>Floor number: </source>
+        <translation>Этаж №</translation>
+    </message>
+</context>
+<context>
+    <name>Form</name>
+    <message>
+        <location filename="startDialog.ui" line="32"/>
+        <source>Maze</source>
+        <translation type="unfinished">Лабиринт</translation>
+    </message>
+    <message>
+        <location filename="startDialog.ui" line="49"/>
+        <source>Login</source>
+        <translation type="unfinished">Логин</translation>
+    </message>
+    <message>
+        <location filename="startDialog.ui" line="62"/>
+        <source>vlad</source>
+        <translation type="unfinished">Влад</translation>
+    </message>
+    <message>
+        <location filename="startDialog.ui" line="78"/>
+        <source>start!</source>
+        <translation type="unfinished">Старт!</translation>
+    </message>
+    <message>
+        <location filename="startDialog.ui" line="102"/>
+        <source>Ip</source>
+        <translation type="unfinished">Ip</translation>
+    </message>
+    <message>
+        <location filename="startDialog.ui" line="115"/>
+        <source>127.0.0.1</source>
+        <translation type="unfinished">127.0.0.1</translation>
+    </message>
+    <message>
+        <location filename="startDialog.ui" line="136"/>
+        <source>Port</source>
+        <translation type="unfinished">порт</translation>
+    </message>
+    <message>
+        <location filename="startDialog.ui" line="170"/>
+        <source>Skin</source>
+        <translation type="unfinished">Скин</translation>
+    </message>
+    <message>
+        <location filename="startDialog.ui" line="200"/>
+        <source>FullScreen</source>
+        <translation type="unfinished">Полноэкранный режим</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.cpp" line="45"/>
+        <source>z</source>
+        <translation type="unfinished">я</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="53"/>
+        <source>q</source>
+        <translation type="unfinished">й</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="55"/>
+        <source>e</source>
+        <translation type="unfinished">у</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="59"/>
+        <location filename="mainwindow.cpp" line="123"/>
+        <source>w</source>
+        <translation type="unfinished">ц</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="61"/>
+        <location filename="mainwindow.cpp" line="129"/>
+        <source>s</source>
+        <translation type="unfinished">ы</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="67"/>
+        <location filename="mainwindow.cpp" line="131"/>
+        <source>a</source>
+        <translation type="unfinished">ф</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="69"/>
+        <location filename="mainwindow.cpp" line="133"/>
+        <source>d</source>
+        <translation type="unfinished">в</translation>
+    </message>
+</context>
+</TS>
