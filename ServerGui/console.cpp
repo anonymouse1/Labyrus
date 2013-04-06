53aaf6ad05577503e5be3d4e8b1e439a0897c927
Tyulbashev Vladislav Sergeevich <vladislav.tyulbashev@yandex.ru>
2013-04-06 18:02:16 +0400

    No warnings

--- ServerGui/console.cpp
+++ ServerGui/console.cpp
@@ -3,12 +3,12 @@
 Console::Console(QWidget *parent) : QPlainTextEdit(parent) {
 }
 
-void Console::mouseDoubleClickEvent(QMouseEvent *e) {}
-void Console::mousePressEvent(QMouseEvent *e) {}
-void Console::mouseMoveEvent(QMouseEvent *e) {}
-void Console::mouseReleaseEvent(QMouseEvent *e) {}
-void Console::keyPressEvent(QKeyEvent *e) {}
-void Console::keyReleaseEvent(QKeyEvent *e) {}
+void Console::mouseDoubleClickEvent(QMouseEvent *e) {e->accept();}
+void Console::mousePressEvent(QMouseEvent *e) {e->accept();}
+void Console::mouseMoveEvent(QMouseEvent *e) {e->accept();}
+void Console::mouseReleaseEvent(QMouseEvent *e) {e->accept();}
+void Console::keyPressEvent(QKeyEvent *e) {e->accept();}
+void Console::keyReleaseEvent(QKeyEvent *e) {e->accept();}
 
 void Console::addString(QString s) {
     if (s[s.length() - 1] == '\n')
